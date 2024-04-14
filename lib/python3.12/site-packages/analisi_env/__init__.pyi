5af258e0ffa051d77726f523c069e0a6826a0ed6
AliothCancer <ortjuly7@gmail.com>
2024-04-14 12:39:34 +0200

    adding pyi file

---
+++ lib/python3.12/site-packages/analisi_env/__init__.pyi
@@ -0,0 +1,18 @@
+
+
+def sum_as_string(a: int, b: int) -> str:
+    """
+    A function that return the sum of the given numbers
+
+    """
+
+
+def sum_numbers(number: int) -> int:
+    """
+    Return the sum of number from 0 to given number
+    """
+
+def plot(data: list[float]) -> None:
+    """
+    Plot and within an iced window
+    """
